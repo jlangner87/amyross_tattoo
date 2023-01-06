c9a272810e7ca723c92f9716ead530c4fa347411
jlangner87 <josh.langner@gmail.com>
2023-01-06 17:31:39 -0600

    reconfigure nav layout

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -4,6 +4,7 @@ import Nav from './nav'
 function Header() {
   return (
     <div className="Header">
+
       <img src={logo1} className="hero_image" />
       <h1 className="name">Amy Ross</h1>
       <h1 className="job"> tattoo artist</h1>
--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -2,16 +2,14 @@ import { NavLink } from 'react-router-dom'
 import about from '../assets/about.png'
 import gallery from '../assets/gallery.png'
 import contact from '../assets/contact.png'
+import '../styles/Nav.css'
 
 function Nav() {
   return (
-    <div className="Nav">
-
-    <NavLink to={'/contact'}><img src={contact} className="link_icon"/></NavLink>
-    <NavLink to={'/gallery'}><img src={gallery} className="link_icon"/></NavLink>
-    <NavLink to={'/about'}><img src={about} className="link_icon"/></NavLink>
-
-
+    <div className="nav_menu">
+      <NavLink to={'/about'}><img src={about} className="link_icon"/></NavLink>
+      <NavLink to={'/gallery'}><img src={gallery} className="link_icon"/></NavLink>
+      <NavLink to={'/contact'}><img src={contact} className="link_icon"/></NavLink>
 </div>
   )
 }
