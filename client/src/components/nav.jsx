dacdd52575a668db0426b4f54e73339c13f61ee3
jlangner87 <josh.langner@gmail.com>
2023-01-07 11:03:04 -0600

    mobile responsive styling && 2rd iteration of nav

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -7,9 +7,9 @@ import '../styles/Nav.css'
 function Nav() {
   return (
     <div className="nav_menu">
-      <NavLink to={'/about'}><img src={about} className="link_icon"/></NavLink>
-      <NavLink to={'/gallery'}><img src={gallery} className="link_icon"/></NavLink>
-      <NavLink to={'/contact'}><img src={contact} className="link_icon"/></NavLink>
+      <NavLink className='nav_link' to={'/about'}>ABOUT</NavLink>
+      <NavLink className='nav_link' to={'/gallery'}>GALLERY</NavLink>
+      <NavLink className='nav_link' to={'/contact'}>CONTACT</NavLink>
 </div>
   )
 }
